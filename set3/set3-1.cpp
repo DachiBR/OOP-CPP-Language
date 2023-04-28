dc5e4a22c339dd8485c13ef30abd7351719f927d
Dachi Bregadze <75274431+DachiBR@users.noreply.github.com>
2023-04-28 22:53:38 +0400

    Update set3-1.cpp

--- set3/set3-1.cpp
+++ set3/set3-1.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
+#include <utility>
 #include <string>
-#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main() {
+    string input;
+    int count_pairs = 0;
+    pair<string,string> wyvili[5000];
     
-    //When the deadline of this challenges will be passed , The code will be added approximately: 2023-04-30
+    while(getline(cin,input) && !input.empty()){
+        for(int i = 0; i < input.length(); i++){
+            if(input[i] == ' '){
+                wyvili[count_pairs].first = input.substr(0,i);
+                wyvili[count_pairs].second = input.substr(i+1,input.length());
+                count_pairs++;
+            }
+        }
+    }
+    
+    while(getline(cin,input) && !input.empty()){
+        string word = "";
+        string output = "";
+        input += ' '; 
+        for(int i = 0; i < input.length(); i++){
+            if(isalnum(input[i])){ 
+                word += input[i];
+            } else {
+                for(int j = 0; j < count_pairs; j++){
+                    if(wyvili[j].first == word){
+                        output += wyvili[j].second;
+                        word = "";
+                    }
+                }
+                if(word != ""){
+                    output += word;
+                    word = "";
+                }
+                output += input[i];
+            }
+        }       
+        
+        cout << output << endl;
+    }
     
     return 0;
 }
