948b792c9d4116a7507bbb616f7f49e521831ac5
Dachi Bregadze <75274431+DachiBR@users.noreply.github.com>
2023-04-28 22:51:16 +0400

    Update set1-2.cpp

--- set1/set1-2.cpp
+++ set1/set1-2.cpp
@@ -4,54 +4,67 @@
 
 using namespace std;
 
-bool isValidIdentifier(string identifier) {
-    //Check if the first character is an underscore or a letter
-    if (identifier[0] == '_' || (identifier[0] >= 'A' && identifier[0] <= 'Z') || (identifier[0] >= 'a' && identifier[0] <= 'z')) {
-        //Check the other characters
-        for (int i = 1; i < identifier.length(); i++) {
-            //Check if the character is a underscore or letter or a digit
-            if (identifier[i] == '_' || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] >= 'a' && identifier[i] <= 'z') || (identifier[i] >= 48 && identifier[i] <= 57)) {
+bool isValidIdentifier(string identifier)
+{
+    // Check if the first character is an underscore or a letter
+    if (identifier[0] == '_' || (identifier[0] >= 'A' && identifier[0] <= 'Z') || (identifier[0] >= 'a' && identifier[0] <= 'z'))
+    {
+        // Check the other characters
+        for (int i = 1; i < identifier.length(); i++)
+        {
+            // Check if the character is a underscore or letter or a digit
+            if (identifier[i] == '_' || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] >= 'a' && identifier[i] <= 'z') || (identifier[i] >= 48 && identifier[i] <= 57))
+            {
                 continue;
-            } else {
+            }
+            else
+            {
                 return false;
             }
         }
-    } else {
+    }
+    else
+    {
         return false;
     }
 
     return true;
 }
 
-int main() {
+int main()
+{
     string inputLine;
 
-    //Read input 
-    while (getline(cin, inputLine)) {
+    // Read input
+    while (getline(cin, inputLine))
+    {
         inputLine += " ";
         string identifier = "";
         int countIdentifiers = 0;
 
-        //Count the number of valid identifiers in the line
-        for (int i = 0; i < inputLine.length(); i++) {
-            //If the current character is not a space append it to the current identifier
-            if (inputLine[i] != 32) {
+        // Count the number of valid identifiers in the line
+        for (int i = 0; i < inputLine.length(); i++)
+        {
+            // If the current character is not a space, append it to the current identifier
+            if (inputLine[i] != 32)
+            {
                 identifier += inputLine[i];
-            } else {
-                //Check if the current identifier is a valid identifier and increment the count if it is
-                if (isValidIdentifier(identifier)) {
+            }
+            else
+            {
+                // Check if the current identifier is a valid identifier and increment the count if it is
+                if (isValidIdentifier(identifier))
+                {
                     countIdentifiers++;
                 }
-                //Reset the identifier
+                // Reset the identifier
                 identifier = "";
             }
         }
-        //Output the count of valid identifiers in the line
+        // Output the count of valid identifiers in the line
         cout << countIdentifiers << endl;
     }
 
     return 0;
 }
-
-
   
